d588f6d9d37f5d0e28b71dd130cea8b05647b58d
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-09-06 20:41:31 +0000

    Testing shell

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+
+#endif
---
+++ shell.c
@@ -0,0 +1,69 @@
+#include "main.h"
+int main(void)
+{
+    char input[MAX_COMMAND_LENGTH];
+
+    while (1)
+    {
+        printf("simple_shell> ");
+
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            if (feof(stdin))
+            {
+                printf("\nGoodbye!\n");
+                exit(0); // End of file (Ctrl+D) was reached
+            }
+            else
+            {
+                perror("Error reading input");
+                exit(1);
+            }
+        }
+
+        // Remove newline character at the end
+        size_t input_length = strlen(input);
+        if (input_length > 0 && input[input_length - 1] == '\n')
+        {
+            input[input_length - 1] = '\0';
+        }
+
+        if (strcmp(input, "") == 0)
+        {
+            continue; // Empty input, prompt again
+        }
+
+        pid_t pid = fork(); // Create a child process
+
+        if (pid < 0)
+        {
+            perror("Fork failed");
+            exit(1);
+        }
+        else if (pid == 0)
+        {
+            // Child process
+            if (execlp(input, input, NULL) == -1)
+            {
+                perror("Command not found");
+                exit(1);
+            }
+        }
+        else
+        {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+
+            if (WIFEXITED(status))
+	    {
+		int exit_code = WEXITSTATUS(status);
+		if (exit_code != 0)
+		{
+			printf("simple_shell: %s: exited with status %d\n", input, exit_code);
+		}
+		}
+	}
+	}
+	return (0);
+}
