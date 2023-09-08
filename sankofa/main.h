663adba1acce53a2afbfb387b5f024f6aa951ce9
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-09-08 05:56:08 -0700

    all optional task handled

---
+++ sankofa/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 256
+
+void execute_command(const char *input);
+void print_environment();
+#endif
