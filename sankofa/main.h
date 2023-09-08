e62cfa773dda8e3e95973baf9a024d8b5f70fa06
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-09-08 06:31:26 -0700

    This should work with the allowed functions

--- sankofa/main.h
+++ sankofa/main.h
@@ -8,7 +8,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 256
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENTS 64
 
 void execute_command(const char *input);
 void print_environment();
