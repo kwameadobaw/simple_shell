7a8b845289f3d7d5d528a26d9573002cd2ce4615
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-09-10 07:14:16 +0000

    Created getline function

---
+++ _getline.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * my_getline - Read a line of input from the user
+ * @input: The input buffer to store the line
+ * @max_length: The maximum length of the input buffer
+ * Return: The number of characters read, or -1 on error
+ */
+ssize_t my_getline(char *input, size_t max_length)
+{
+	static char buffer[MAX_COMMAND_LENGTH];
+	static size_t buffer_position = 0;
+	static size_t buffer_size = 0;
+	size_t i = 0;
+	char c;
+
+	while (i < max_length - 1)
+	{
+		if (buffer_position >= buffer_size)
+		{
+			buffer_size = read(STDIN_FILENO, buffer, sizeof(buffer));
+			if (buffer_size <= 0)
+				return (buffer_size);
+			buffer_position = 0;
+		}
+		c = buffer[buffer_position++];
+		input[i++] = c;
+		if (c == '\n')
+			break;
+	}
+	input[i] = '\0';
+	return (i);
+}
--- main.h
+++ main.h
@@ -29,4 +29,5 @@ void print_environment()
 }
 void execute_command(const char *input);
 void print_environment();
+ssize_t my_getline(char *input, size_t max_length);
 #endif
