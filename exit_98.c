4e6434e91eb892cecacdd813df2e2b4db52ddd95
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-09-10 14:41:48 -0700

    tried exit status but not working

---
+++ exit_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * handle_exit - Handle the "exit" command with an optional status code
+ * @input: The input string containing the status code (if provided)
+ *
+ * This function handles the "exit" command in a simple shell. If the input
+ * sting is "exit", it exists the shell with a status code of 0. If the input
+ * string is "exit <status>", it converts the status string to an integer and
+ * exists the shell with the specified status code.
+*/
+void handle_exit(const char *input)
+{
+	int status;
+
+	status = atoi(input);
+
+	exit(status);
+}
