83ec87290dfebf8d369766d43d8ce75e0719a603
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-09-20 21:00:43 +0000

    Brought back while loop

--- shell.c
+++ shell.c
@@ -51,40 +51,42 @@ void execute_command(const char *input)
  */
 int main(void)
 {
-	char input[MAX_COMMAND_LENGTH];
-	size_t input_length;
+	while (1)
+	{
+		char input[MAX_COMMAND_LENGTH];
+		size_t input_length;
 
-	write(STDOUT_FILENO, "simple_shell> ", 14);
-	if (my_getline(input, sizeof(input)) == -1)
-	{
-		perror("Error reading input");
-		exit(1);
-	}
-	input_length = _strlen(input);
+		write(STDOUT_FILENO, "simple_shell> ", 14);
+		if (my_getline(input, sizeof(input)) == -1)
+		{
+			perror("Error reading input");
+			exit(1);
+		}
+		input_length = _strlen(input);
 
-	if (input_length > 0 && input[input_length - 1] == '\n')
-	{
-		input[input_length - 1] = '\0';
-	}
-	if (_strcmp(input, "") == 0)
-	{
-		return (0);
-	}
-	if (_strcmp(input, "exit") == 0)
-	{
-		write(STDOUT_FILENO, "Goodbye!\n", 9);
-		exit(0);
-	}
-	if (_strncmp(input, "exit ", 5) == 0)
-	{
-		int status = _atoi(input + 5);
+		if (input_length > 0 && input[input_length - 1] == '\n')
+		{
+			input[input_length - 1] = '\0';
+		}
+		if (_strcmp(input, "") == 0)
+		{
+			return (0);
+		}
+		if (_strcmp(input, "exit") == 0)
+		{
+			write(STDOUT_FILENO, "Goodbye!\n", 9);
+			exit(0);
+		}
+		if (_strncmp(input, "exit ", 5) == 0)
+		{
+			int status = _atoi(input + 5);
 
-		exit(status);
+			exit(status);
+		}
+		else
+		{
+			execute_command(input);
+		}
 	}
-	else
-	{
-		execute_command(input);
-	}
-
 	return (0);
 }
